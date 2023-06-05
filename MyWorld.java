ae7ec2826dc439337619597e16652e5d5c393728
YusufA2 <349990275@gapps.yrdsb.ca>
2023-06-05 08:31:40 -0400

    Snake 1.01

--- MyWorld.java
+++ MyWorld.java
@@ -17,6 +17,10 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        getBackground().setColor(Color.BLACK);
+        getBackground().fill();
+        
     }
-    Snake snake = new Snake 
+    
+    
 }
